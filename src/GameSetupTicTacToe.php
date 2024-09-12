2395ebfbf03260630ca9692091140307444055e7
mdram83 <michal.dramowicz@gmail.com>
2024-09-12 12:58:16 +0200

    GameSetupTicTacToe.php

---
+++ src/GameSetupTicTacToe.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace MyDramGames\Games\TicTacToe;
+
+use MyDramGames\Core\GameOption\GameOptionTypeGeneric;
+use MyDramGames\Core\GameOption\GameOptionValueCollection;
+use MyDramGames\Core\GameOption\Options\GameOptionAutostartGeneric;
+use MyDramGames\Core\GameOption\Options\GameOptionForfeitAfterGeneric;
+use MyDramGames\Core\GameOption\Options\GameOptionNumberOfPlayersGeneric;
+use MyDramGames\Core\GameOption\Values\GameOptionValueAutostartGeneric;
+use MyDramGames\Core\GameOption\Values\GameOptionValueForfeitAfterGeneric;
+use MyDramGames\Core\GameOption\Values\GameOptionValueNumberOfPlayersGeneric;
+use MyDramGames\Core\GameSetup\GameSetup;
+use MyDramGames\Core\GameSetup\GameSetupBase;
+
+class GameSetupTicTacToe extends GameSetupBase implements GameSetup
+{
+    /**
+     * @inheritDoc
+     */
+    protected function prepareDefaultOptions(GameOptionValueCollection $valuesHandler): array
+    {
+        return
+            [
+                new GameOptionNumberOfPlayersGeneric(
+                    $valuesHandler->clone()->reset([
+                        GameOptionValueNumberOfPlayersGeneric::Players002,
+                    ]),
+                    GameOptionValueNumberOfPlayersGeneric::Players002,
+                    GameOptionTypeGeneric::Radio
+                ),
+
+                new GameOptionAutostartGeneric(
+                    $valuesHandler->clone()->reset([
+                        GameOptionValueAutostartGeneric::Enabled,
+                        GameOptionValueAutostartGeneric::Disabled
+                    ]),
+                    GameOptionValueAutostartGeneric::Enabled,
+                    GameOptionTypeGeneric::Checkbox
+                ),
+
+                new GameOptionForfeitAfterGeneric(
+                    $valuesHandler->clone()->reset([
+                        GameOptionValueForfeitAfterGeneric::Disabled,
+                        GameOptionValueForfeitAfterGeneric::Minute,
+                    ]),
+                    GameOptionValueForfeitAfterGeneric::Disabled,
+                    GameOptionTypeGeneric::Checkbox
+                ),
+
+            ];
+    }
+}
---
+++ tests/GameSetupTicTacToeTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests;
+
+use MyDramGames\Core\GameOption\GameOptionCollectionPowered;
+use MyDramGames\Core\GameOption\GameOptionValueCollectionPowered;
+use MyDramGames\Core\GameOption\Values\GameOptionValueAutostartGeneric;
+use MyDramGames\Core\GameOption\Values\GameOptionValueForfeitAfterGeneric;
+use MyDramGames\Games\TicTacToe\GameSetupTicTacToe;
+use PHPUnit\Framework\TestCase;
+
+class GameSetupTicTacToeTest extends TestCase
+{
+    protected GameSetupTicTacToe $setup;
+
+    public function setUp(): void
+    {
+        $this->setup = new GameSetupTicTacToe(
+            new GameOptionCollectionPowered(),
+            new GameOptionValueCollectionPowered()
+        );
+    }
+
+    public function testGetAllOptions(): void
+    {
+        $allOptions = $this->setup->getAllOptions();
+        $numberOfPlayers = $allOptions->getOne('numberOfPlayers');
+        $autostart = $allOptions->getOne('autostart');
+        $forfeitAfter = $allOptions->getOne('forfeitAfter');
+
+        $this->assertEquals(3, $allOptions->count());
+
+        $this->assertEquals(1, $numberOfPlayers->getAvailableValues()->count());
+        $this->assertEquals(2, $numberOfPlayers->getAvailableValues()->pullFirst()->getValue());
+        $this->assertEquals(2, $numberOfPlayers->getDefaultValue()->getValue());
+
+        $this->assertEquals(2, $autostart->getAvailableValues()->count());
+        $this->assertEquals(GameOptionValueAutostartGeneric::Enabled->value, $autostart->getAvailableValues()->pullFirst()->getValue());
+        $this->assertEquals(GameOptionValueAutostartGeneric::Enabled->value, $autostart->getDefaultValue()->getValue());
+
+        $this->assertEquals(2, $forfeitAfter->getAvailableValues()->count());
+        $this->assertEquals(GameOptionValueForfeitAfterGeneric::Disabled->value, $forfeitAfter->getAvailableValues()->pullFirst()->getValue());
+        $this->assertEquals(GameOptionValueForfeitAfterGeneric::Disabled->value, $forfeitAfter->getDefaultValue()->getValue());
+    }
+}
